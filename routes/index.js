74ac678f692f04fdd6677de3438eb1a7f3bd6f6d
James Lake <jh.lake@uniandes.edu.co>
2019-11-06 16:29:15 -0500

    button layout basic

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html', { title: 'This is a Placeholder' });
+  res.render('index.html', { title: 'Tesis Maletas' });
+});
+router.get('/all', function(req, res, next) {
+  res.render('todaInformacion.html', { title: 'Tesis Maletas' });
+});
+router.get('/alertas', function(req, res, next) {
+  res.render('alertas.html', { title: 'Tesis Maletas' });
+});
+router.get('/ubicaciones', function(req, res, next) {
+  res.render('ubicaciones.html', { title: 'Tesis Maletas' });
+});
+router.get('/configurar', function(req, res, next) {
+  res.render('configuracionVuelo.html', { title: 'Tesis Maletas' });
 });
 
 module.exports = router;
