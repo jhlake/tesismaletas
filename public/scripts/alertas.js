536c7d46ce45c1a8f6c731e1944bfaa9d7dabb8b
jhon <jj.gonzalez10@uniandes.edu.co>
2019-11-27 14:47:44 -0500

    correcion cosas alertas

--- public/scripts/alertas.js
+++ public/scripts/alertas.js
@@ -6,8 +6,13 @@ request.send();
 
 request.onload = function() {
 var dataObject = request.response;
-var dataObject2 = new Array();
-var count = Object.keys(dataObject).length
+var dataObject2 = [];
+var jsonSensores = [];
+sensores();
+
+function sensores()
+{
+  var count = Object.keys(dataObject).length
 //console.log(count)
 for(let i = 0; i < count; i++){
 
@@ -16,7 +21,9 @@ for(let i = 0; i < count; i++){
   //console.log(count2);
   for(let j = 0; j < count2; j++){
   
+    
     let actual2 = JSON.stringify(actual);
+    console.log()
     let splits = actual2.split(",");
     var lectura = splits[0];
     var sensor = splits[1];
@@ -24,9 +31,13 @@ for(let i = 0; i < count; i++){
     var unit = splits[3];
     var time = splits[4];
     var door = new String ("Door");
+
     //console.log("sensor"+ sensor);
 
     // sensoresssssss
+
+    //con esto funciona
+    /*
     var sensor2 = JSON.stringify(sensor);
     var splitsSensor= sensor2.split('\\"');
     var s1 = splitsSensor[1];
@@ -36,6 +47,14 @@ for(let i = 0; i < count; i++){
     var splitsValue= value2.split('\\"');
     var val1 = splitsValue[1];
     var val2 = splitsValue[3];
+    */
+    //
+    var splitsSensor= sensor.split('"');
+    var s1 = splitsSensor[1];
+    var s2 = splitsSensor[3];
+    var splitsValue= value.split('"');
+    var val1 = splitsValue[1];
+    var val2 = splitsValue[3];
 
     // puerta abierta 
     if (s2 === "Door" && val2 === "abierto")
@@ -43,15 +62,19 @@ for(let i = 0; i < count; i++){
       //console.log(s2 + val2);
       //console.log(actual2);
       dataObject2.push(actual2);
-      //console.log(dataObject2);
+
+
     }
     // sensoressssss
   }
 
 }
+}
+jsonSensores = JSON.parse(JSON.stringify(dataObject2));
+console.log(jsonSensores);
 
 var listItemString = $('#listItem').html();
-  
+
 //dataObject.forEach(buildNewList);
 dataObject.forEach(buildNewList);
 
