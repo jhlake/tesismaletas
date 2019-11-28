c4e9300084a4a73ced042b4c594f2419b433ac8e
jhon <jj.gonzalez10@uniandes.edu.co>
2019-11-28 15:47:01 -0500

    gps

--- public/scripts/alertas.js
+++ public/scripts/alertas.js
@@ -56,18 +56,38 @@ for(let i = 0; i < count; i++){
       var max = new Number('18');
       if (x > max || y > max || z > max)
       {
-        console.log(x + "/" + y + "/" + z);
+        //console.log(x + "/" + y + "/" + z);
         dataObject2.push(JSON.parse(actual2));
       }
     }
     if (s2 ==="GPS")
     {
-      
+      getDistanceFromLatLonInKm(25.249317,-109.906486,23.496641,-107.719159);
+
     }
 
 }
 }
 
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  console.log(d);
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
 
 var listItemString = $('#listItem').html();
 
